a65b235fa3a5e2e46ae878475fb877584d957207
Nehemiah Nesanathan <144255177+NrGjoe846@users.noreply.github.com>
2024-11-26 06:22:13 +0000

    Create app.tsx
    gowtham
    rs file

---
+++ src/store/app.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { LearningHub } from './components/LearningHub';
+
+export function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <LearningHub />
+    </div>
+  );
+}
